6d1a4b0623ac361577592711689f40d741d98ac8
Mark <mark005@pacbell.net>
2014-01-24 19:22:39 -0800

    Added toggle between arcade and tank drive

--- src/edu/wpi/first/wpilibj/templates/FRC2014.java
+++ src/edu/wpi/first/wpilibj/templates/FRC2014.java
@@ -3,13 +3,13 @@ package edu.wpi.first.wpilibj.templates;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.DriverStationLCD;
 
 /*
-    FRC2014 Code
-    Document your changes in your git commit messages
-    View commit messages by right clicking in project folder and selecting git log.
-*/
-
+ FRC2014 Code
+ Document your changes in your git commit messages
+ View commit messages by right clicking in project folder and selecting git log.
+ */
 public class FRC2014 extends SimpleRobot {
     // <editor-fold defaultstate="collapsed" desc="Variable DefinitionS">
 
@@ -25,11 +25,17 @@ public class FRC2014 extends SimpleRobot {
     final int JOYSTICK_RIGHT_USB = 2;
     final int JOYSTICK_OPERATOR_USB = 3;
 
+    //defining joystick buttons
+    final int JOYSTICK_TOGGLE_DRIVE = 3;
+
     //defining others
     RobotDrive driver;
+    DriverStationLCD lcd;
     Joystick joyLeft;
     Joystick joyRight;
     Joystick joyOperator;
+    boolean isTankDrive; //true is tank drive, false is arcade drive
+    final String VERSION_NUMBER = "0.1";
 
     // </editor-fold>
     /**
@@ -37,16 +43,21 @@ public class FRC2014 extends SimpleRobot {
      */
     public FRC2014() {
         driver = new RobotDrive(MOTOR_LEFT_PWM, MOTOR_RIGHT_PWM);
+        lcd = DriverStationLCD.getInstance();
 
         joyLeft = new Joystick(JOYSTICK_LEFT_USB);
         joyRight = new Joystick(JOYSTICK_RIGHT_USB);
         joyOperator = new Joystick(JOYSTICK_OPERATOR_USB);
+        
+        isTankDrive = true;
     }
 
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
+        lcd.println(DriverStationLCD.Line.kUser1, 1, "autonomous v"+VERSION_NUMBER);
+        lcd.updateLCD();
         while (isAutonomous() && isEnabled()) {
 
         }
@@ -56,8 +67,27 @@ public class FRC2014 extends SimpleRobot {
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
+        lcd.println(DriverStationLCD.Line.kUser1, 1, "teleoperated v"+VERSION_NUMBER);
+        lcd.updateLCD();
+        boolean oldToggleDriveValue = false;
+
         while (isOperatorControl() && isEnabled()) {
-            driver.tankDrive(joyLeft, joyRight);
+            
+            boolean toggleDriveValue = joyLeft.getRawButton(JOYSTICK_TOGGLE_DRIVE);
+            if (toggleDriveValue && (oldToggleDriveValue == false)) { //detects state change
+                isTankDrive = !isTankDrive;
+            }
+            if (isTankDrive) {
+                driver.tankDrive(joyLeft, joyRight);
+                lcd.println(DriverStationLCD.Line.kUser2, 1, "Tank Drive");
+                lcd.updateLCD();
+            } else {
+                driver.arcadeDrive(joyLeft);
+                lcd.println(DriverStationLCD.Line.kUser2, 1, "Arcade Drive");
+                lcd.updateLCD();
+            }
+            oldToggleDriveValue = toggleDriveValue;
+
         }
     }
 
@@ -65,6 +95,8 @@ public class FRC2014 extends SimpleRobot {
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
+        lcd.println(DriverStationLCD.Line.kUser1, 1, "test v"+VERSION_NUMBER);
+        lcd.updateLCD();
         while (isTest() && isEnabled()) {
 
         }
