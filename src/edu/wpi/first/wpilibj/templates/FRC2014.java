37b1bbedb056d50b51530ef6870e77a47607f15b
Mark <mark005@pacbell.net>
2014-01-29 18:44:19 -0800

    Made changes to kicker state machine and added compressor

--- src/edu/wpi/first/wpilibj/templates/FRC2014.java
+++ src/edu/wpi/first/wpilibj/templates/FRC2014.java
@@ -1,5 +1,7 @@
 package edu.wpi.first.wpilibj.templates;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import edu.wpi.first.wpilibj.RobotDrive;
@@ -17,12 +19,18 @@ public class FRC2014 extends SimpleRobot {
     //defining pwm constants
     static final int MOTOR_LEFT_PWM = 1;
     static final int MOTOR_RIGHT_PWM = 8;
+    static final int MOTOR_KICK_PWM = 3;
 
     //defining digital io constants
     static final int PRESSURE_SENSOR_PORT = 4;
     static final int KICKER_ENCODER_PORT_A = 6;
     static final int KICKER_ENCODER_PORT_B = 7;
     static final int KICKER_OPTICAL_SENSOR_PORT = 2;
+    static final int SOLENOID_SHIFT_FORWARD_PORT = 3;
+    static final int SOLENOID_SHIFT_BACKWARD_PORT = 4;
+
+    //defining relay constants
+    static final int SPIKE_PRESSURE_RELAY = 1;
 
     //defining joystick numbers
     static final int JOYSTICK_LEFT_USB = 1;
@@ -33,13 +41,19 @@ public class FRC2014 extends SimpleRobot {
     static final int JOYSTICK_TOGGLE_DRIVE = 3;
     static final int JOYSTICK_LOAD_BUTTON = 3;
     static final int JOYSTICK_FIRE_BUTTON = 1;
+    static final int JOYSTICK_RESET_BUTTON = 2;
+    static final int SOLENOID_OUT_BUTTON = 5;
+    static final int SOLENOID_IN_BUTTON = 4;
 
     //defining encoder positions
-    static final int KICKER_ENCODER_TOP_POSITION = 125;
-    static final int KICKER_ENCODER_KICK_POSITION = -31;
+    static final int KICKER_ENCODER_TOP_POSITION = 55; //loading is positive
+    static final int KICKER_ENCODER_KICK_POSITION = -16; //kick is negative
     static final int KICKER_ENCODER_REST_POSITION = 0;
-    
-    
+
+    //defining pneumatic objects
+    private Compressor compress;
+    private DoubleSolenoid testSolenoid;
+
     //defining others
     private RobotDrive driver;
     private DriverStationLCD lcd;
@@ -59,14 +73,20 @@ public class FRC2014 extends SimpleRobot {
         driver = new RobotDrive(MOTOR_LEFT_PWM, MOTOR_RIGHT_PWM);
         lcd = DriverStationLCD.getInstance();
         ds = DriverStation.getInstance();
-
+        
         joyLeft = new Joystick(JOYSTICK_LEFT_USB);
         joyRight = new Joystick(JOYSTICK_RIGHT_USB);
         joyOperator = new Joystick(JOYSTICK_OPERATOR_USB);
-
+        
         isTankDrive = true;
         
         kickerStates = new KickerStateMachine();
+        
+        compress = new Compressor(PRESSURE_SENSOR_PORT, SPIKE_PRESSURE_RELAY);
+        //comment compressor if you are not using it
+        //compress.start();
+        
+        testSolenoid = new DoubleSolenoid(SOLENOID_SHIFT_FORWARD_PORT, SOLENOID_SHIFT_BACKWARD_PORT);
     }
 
     /**
@@ -76,7 +96,7 @@ public class FRC2014 extends SimpleRobot {
         lcd.println(DriverStationLCD.Line.kUser1, 1, "autonomous v" + VERSION_NUMBER);
         lcd.updateLCD();
         while (isAutonomous() && isEnabled()) {
-
+            
         }
     }
 
@@ -87,9 +107,9 @@ public class FRC2014 extends SimpleRobot {
         lcd.println(DriverStationLCD.Line.kUser1, 1, "teleoperated v" + VERSION_NUMBER);
         lcd.updateLCD();
         boolean oldToggleDriveValue = false;
-
+        
         while (isOperatorControl() && isEnabled()) {
-
+            
             boolean toggleDriveValue = joyLeft.getRawButton(JOYSTICK_TOGGLE_DRIVE);
             if (toggleDriveValue && (oldToggleDriveValue == false)) { //detects state change
                 isTankDrive = !isTankDrive;
@@ -103,8 +123,13 @@ public class FRC2014 extends SimpleRobot {
                 lcd.println(DriverStationLCD.Line.kUser2, 1, "Arcade Drive");
                 lcd.updateLCD();
             }
+            if (joyOperator.getRawButton(SOLENOID_OUT_BUTTON)) {
+                testSolenoid.set(DoubleSolenoid.Value.kForward);
+            } else if (joyOperator.getRawButton(SOLENOID_IN_BUTTON)) {
+                testSolenoid.set(DoubleSolenoid.Value.kReverse);
+            }
             oldToggleDriveValue = toggleDriveValue;
-
+            
         }
     }
 
@@ -114,6 +139,7 @@ public class FRC2014 extends SimpleRobot {
     public void test() {
         lcd.println(DriverStationLCD.Line.kUser1, 1, "test v" + VERSION_NUMBER);
         lcd.updateLCD();
+        kickerStates.setState(kickerStates.INIT); //makes sure robot is on init
         while (isTest() && isEnabled()) {
             kickerStates.stateMachine();
         }
--- src/edu/wpi/first/wpilibj/templates/KickerStateMachine.java
+++ src/edu/wpi/first/wpilibj/templates/KickerStateMachine.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.DriverStationLCD;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Talon;
 
 /*
 states:
@@ -15,17 +16,20 @@ wait
 kick
 back drive
 encoder reset
+reset: not called every time
 */
 
 public class KickerStateMachine {
     
     // <editor-fold defaultstate="collapsed" desc="Variable Definitions">
     
-    public final int INIT = 0, MOTOR_ON = 1, MOTOR_MOVING_UP = 2, MOTOR_OFF = 3, WAIT = 4, KICK = 5, BACK_DRIVE = 6, ENCODER_RESET = 7;
+    public final int INIT = 0, MOTOR_ON = 1, MOTOR_MOVING_UP = 2, MOTOR_OFF = 3, WAIT = 4, KICK = 5, BACK_DRIVE = 6, ENCODER_RESET = 7, RESET = 8;
     private int state = INIT;
     private Joystick joyOperator;
     private Encoder kickerEncoder;
     private DigitalInput kickerOpticalSensor;
+    //public Talon kickerMotor;
+    private Talon kickerMotor;
     
     private DriverStationLCD lcd;
     
@@ -36,6 +40,7 @@ public class KickerStateMachine {
         kickerEncoder = new Encoder(FRC2014.KICKER_ENCODER_PORT_A, FRC2014.KICKER_ENCODER_PORT_B);
         kickerOpticalSensor = new DigitalInput(FRC2014.KICKER_OPTICAL_SENSOR_PORT);
         kickerEncoder.start();
+        kickerMotor = new Talon(FRC2014.MOTOR_KICK_PWM);
         lcd = DriverStationLCD.getInstance();
     }
     
@@ -48,12 +53,16 @@ public class KickerStateMachine {
     }
     
     public void stateMachine() {
-        lcd.println(DriverStationLCD.Line.kUser2, 1, ""+kickerEncoder.get());
+        double motorSpeed;
+        lcd.println(DriverStationLCD.Line.kUser2, 1, ""+kickerEncoder.get()+"   ");
         lcd.updateLCD();
         lcd.println(DriverStationLCD.Line.kUser3, 1, ""+state);
         lcd.updateLCD();
-        lcd.println(DriverStationLCD.Line.kUser4, 1, ""+kickerOpticalSensor.get());
+        lcd.println(DriverStationLCD.Line.kUser4, 1, ""+kickerOpticalSensor.get()+"   ");
         lcd.updateLCD();
+        if(joyOperator.getRawButton(FRC2014.JOYSTICK_RESET_BUTTON)) {
+            state = RESET;
+        }
         switch (state) {
             case INIT:
                 if(joyOperator.getRawButton(FRC2014.JOYSTICK_LOAD_BUTTON)) {
@@ -62,19 +71,23 @@ public class KickerStateMachine {
                 break;
                 
             case MOTOR_ON:
-                //turn motor on
+                //kickerMotor.set();
                 //add motor encoder safety
                 state = MOTOR_MOVING_UP;
                 break;
             
             case MOTOR_MOVING_UP:
+                motorSpeed = ( (joyOperator.getZ() + 1 ) / 2 );
+                kickerMotor.set(motorSpeed); //loading is negative, flip value
+                lcd.println(DriverStationLCD.Line.kUser5, 1, ""+motorSpeed+"   ");
+                lcd.updateLCD();
                 if(kickerEncoder.get() >= FRC2014.KICKER_ENCODER_TOP_POSITION) {
                     state = MOTOR_OFF;
                 }
                 break;
                 
             case MOTOR_OFF:
-                //turn motor off
+                kickerMotor.set(0);
                 state = WAIT;
                 break;
             
@@ -85,14 +98,20 @@ public class KickerStateMachine {
                 break;
                
             case KICK:
-                //kick position negative
+                motorSpeed = - ( (joyOperator.getZ() + 1 ) / 2 );
+                lcd.println(DriverStationLCD.Line.kUser5, 1, ""+motorSpeed+"   ");
+                lcd.updateLCD();
+                kickerMotor.set(motorSpeed); //kicking is positive, flip value
                 if(kickerEncoder.get() <= FRC2014.KICKER_ENCODER_KICK_POSITION) {
                     state = BACK_DRIVE;
                 }
                 break;
                
             case BACK_DRIVE:
-                //backdrive motors
+                motorSpeed = ( (joyOperator.getZ() + 1 ) / 2 );
+                lcd.println(DriverStationLCD.Line.kUser5, 1, ""+motorSpeed+"   ");
+                lcd.updateLCD();
+                kickerMotor.set(motorSpeed); //kicking is positive, flip value
                 if(kickerEncoder.get() >= FRC2014.KICKER_ENCODER_REST_POSITION) {
                     state = ENCODER_RESET;
                 }
@@ -100,12 +119,17 @@ public class KickerStateMachine {
                 
             case ENCODER_RESET:
                 if(!kickerOpticalSensor.get()) {
-                    //turn off motors
+                    kickerMotor.set(0);
                     kickerEncoder.reset();
                     state = INIT;
                 }
                 break;
                
+            case RESET:
+                kickerEncoder.reset();
+                state = INIT;
+                break;
+                
             default:
                 System.out.println("error");
                 state = INIT;
