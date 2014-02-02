a3b8f403b6ffed621010c60834e6596b3b5ea03f
Kathryn1234 <poeschel.k@gmail.com>
2014-02-02 14:08:43 -0800

    Changed drive code to have 4 motors and shifting solenoids. Other miscellaneous changes.

--- src/edu/wpi/first/wpilibj/templates/FRC2014.java
+++ src/edu/wpi/first/wpilibj/templates/FRC2014.java
@@ -14,11 +14,13 @@ import edu.wpi.first.wpilibj.DriverStation;
  View commit messages by right clicking in project folder and selecting git log.
  */
 public class FRC2014 extends SimpleRobot {
-    // <editor-fold defaultstate="collapsed" desc="Variable DefinitionS">
+    // <editor-fold defaultstate="collapsed" desc="Variable Definitions">
 
     //defining pwm constants
-    static final int MOTOR_LEFT_PWM = 1;
-    static final int MOTOR_RIGHT_PWM = 8;
+    static final int MOTOR_FRONT_RIGHT_PWM = 5;
+    static final int MOTOR_FRONT_LEFT_PWM = 2;
+    static final int MOTOR_BACK_RIGHT_PWM = 8;
+    static final int MOTOR_BACK_LEFT_PWM = 1;
     static final int MOTOR_KICK_PWM = 3;
 
     //defining digital io constants
@@ -26,8 +28,10 @@ public class FRC2014 extends SimpleRobot {
     static final int KICKER_ENCODER_PORT_A = 6;
     static final int KICKER_ENCODER_PORT_B = 7;
     static final int KICKER_OPTICAL_SENSOR_PORT = 2;
-    static final int SOLENOID_SHIFT_FORWARD_PORT = 3;
-    static final int SOLENOID_SHIFT_BACKWARD_PORT = 4;
+    static final int SOLENOID_LEFT_SHIFT_HIGH_PORT = 2;
+    static final int SOLENOID_LEFT_SHIFT_LOW_PORT = 1;
+    static final int SOLENOID_RIGHT_SHIFT_HIGH_PORT = 4;
+    static final int SOLENOID_RIGHT_SHIFT_LOW_PORT = 3;
 
     //defining relay constants
     static final int SPIKE_PRESSURE_RELAY = 1;
@@ -42,8 +46,8 @@ public class FRC2014 extends SimpleRobot {
     static final int JOYSTICK_LOAD_BUTTON = 3;
     static final int JOYSTICK_FIRE_BUTTON = 1;
     static final int JOYSTICK_RESET_BUTTON = 2;
-    static final int SOLENOID_OUT_BUTTON = 5;
-    static final int SOLENOID_IN_BUTTON = 4;
+    static final int JOYSTICK_HIGH_SHIFT_BUTTON = 4;
+    static final int JOYSTICK_LOW_SHIFT_BUTTON = 5;
     static final int SET_SAMPLE_RATE_BUTTON = 6;
 
     //defining encoder positions
@@ -53,7 +57,8 @@ public class FRC2014 extends SimpleRobot {
 
     //defining pneumatic objects
     private Compressor compress;
-    private DoubleSolenoid testSolenoid;
+    private DoubleSolenoid leftDriveSolenoid;
+    private DoubleSolenoid rightDriveSolenoid;
 
     //defining others
     private RobotDrive driver;
@@ -71,23 +76,24 @@ public class FRC2014 extends SimpleRobot {
      * This function is called as soon as the robot is enabled.
      */
     public FRC2014() {
-        driver = new RobotDrive(MOTOR_LEFT_PWM, MOTOR_RIGHT_PWM);
+        driver = new RobotDrive(MOTOR_FRONT_LEFT_PWM, MOTOR_BACK_LEFT_PWM, MOTOR_FRONT_RIGHT_PWM, MOTOR_BACK_RIGHT_PWM);
         lcd = DriverStationLCD.getInstance();
         ds = DriverStation.getInstance();
-        
+
         joyLeft = new Joystick(JOYSTICK_LEFT_USB);
         joyRight = new Joystick(JOYSTICK_RIGHT_USB);
         joyOperator = new Joystick(JOYSTICK_OPERATOR_USB);
-        
+
         isTankDrive = true;
-        
+
         //kickerStates = new KickerStateMachine();
-        
         compress = new Compressor(PRESSURE_SENSOR_PORT, SPIKE_PRESSURE_RELAY);
         //comment compressor if you are not using it
-        //compress.start();
-        
-        testSolenoid = new DoubleSolenoid(SOLENOID_SHIFT_FORWARD_PORT, SOLENOID_SHIFT_BACKWARD_PORT);
+        compress.start();
+
+        leftDriveSolenoid = new DoubleSolenoid(SOLENOID_LEFT_SHIFT_HIGH_PORT, SOLENOID_LEFT_SHIFT_LOW_PORT);
+        rightDriveSolenoid = new DoubleSolenoid(SOLENOID_RIGHT_SHIFT_HIGH_PORT, SOLENOID_RIGHT_SHIFT_LOW_PORT);
+
     }
 
     /**
@@ -96,26 +102,40 @@ public class FRC2014 extends SimpleRobot {
     public void autonomous() {
         lcd.println(DriverStationLCD.Line.kUser1, 1, "autonomous v" + VERSION_NUMBER);
         lcd.updateLCD();
-        RobotVision.ResultReport results;
-        for(int i=0; i<3; i++) {
-            results = RobotVision.cameraVision();
-            if(results.targetExists && results.isHot){
-                kickerStates.reset();
-                kickerStates.setState(kickerStates.KICK);
-                while(kickerStates.getState()!=kickerStates.INIT) {
-                    kickerStates.stateMachine();
-                }
-                break;
+        /*RobotVision.ResultReport results;
+         for(int i=0; i<3; i++) {
+         results = RobotVision.cameraVision();
+         if(results.targetExists && results.isHot){
+         kickerStates.reset();
+         kickerStates.setState(kickerStates.KICK);
+         while(kickerStates.getState()!=kickerStates.INIT) {
+         kickerStates.stateMachine();
+         }
+         break;
+         }
+         else {
+         //moveCamera();
+         }
+         }
+         //turnFortyFiveDegrees();
+         kickerStates.reset();
+         kickerStates.setState(kickerStates.KICK);
+         while(kickerStates.getState()!=kickerStates.INIT) {
+         kickerStates.stateMachine();
+         }*/
+        while (isAutonomous() && isEnabled()) {
+            RobotVision.ResultReport results = RobotVision.cameraVision();
+            lcd.println(DriverStationLCD.Line.kUser2, 1, "target is " + results.targetExists);
+            lcd.updateLCD();
+            lcd.println(DriverStationLCD.Line.kUser2, 1, "hot : " + results.isHot);
+            lcd.updateLCD();
+            lcd.println(DriverStationLCD.Line.kUser2, 1, "distance is " + results.distance);
+            lcd.updateLCD();
+            try {
+                wait(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            else {
-                //moveCamera();
-            }
-        }
-        //turnFortyFiveDegrees();
-        kickerStates.reset();
-        kickerStates.setState(kickerStates.KICK);
-        while(kickerStates.getState()!=kickerStates.INIT) {
-            kickerStates.stateMachine();
         }
     }
 
@@ -126,29 +146,31 @@ public class FRC2014 extends SimpleRobot {
         lcd.println(DriverStationLCD.Line.kUser1, 1, "teleoperated v" + VERSION_NUMBER);
         lcd.updateLCD();
         boolean oldToggleDriveValue = false;
-        
+
         while (isOperatorControl() && isEnabled()) {
-            
+
             boolean toggleDriveValue = joyLeft.getRawButton(JOYSTICK_TOGGLE_DRIVE);
             if (toggleDriveValue && (oldToggleDriveValue == false)) { //detects state change
                 isTankDrive = !isTankDrive;
             }
             if (isTankDrive) {
                 driver.tankDrive(joyLeft, joyRight);
-                lcd.println(DriverStationLCD.Line.kUser2, 1, "Tank Drive");
+                lcd.println(DriverStationLCD.Line.kUser2, 1, "Tank Drive    ");
                 lcd.updateLCD();
             } else {
                 driver.arcadeDrive(joyLeft);
                 lcd.println(DriverStationLCD.Line.kUser2, 1, "Arcade Drive");
                 lcd.updateLCD();
             }
-            if (joyOperator.getRawButton(SOLENOID_OUT_BUTTON)) {
-                testSolenoid.set(DoubleSolenoid.Value.kForward);
-            } else if (joyOperator.getRawButton(SOLENOID_IN_BUTTON)) {
-                testSolenoid.set(DoubleSolenoid.Value.kReverse);
-            }
             oldToggleDriveValue = toggleDriveValue;
-            
+
+            if (joyLeft.getRawButton(JOYSTICK_HIGH_SHIFT_BUTTON)) {
+                leftDriveSolenoid.set(DoubleSolenoid.Value.kForward);
+                rightDriveSolenoid.set(DoubleSolenoid.Value.kForward);
+            } else if (joyRight.getRawButton(JOYSTICK_LOW_SHIFT_BUTTON)) {
+                leftDriveSolenoid.set(DoubleSolenoid.Value.kReverse);
+                rightDriveSolenoid.set(DoubleSolenoid.Value.kReverse);
+            }
         }
     }
 
@@ -160,13 +182,13 @@ public class FRC2014 extends SimpleRobot {
         lcd.updateLCD();
         while (isTest() && isEnabled()) {
             RobotVision.ResultReport results = RobotVision.cameraVision();
-            lcd.println(DriverStationLCD.Line.kUser2, 1, "target is "+results.targetExists);
+            lcd.println(DriverStationLCD.Line.kUser2, 1, "target is " + results.targetExists);
         }
         /*kickerStates.setSetpoint(KICKER_ENCODER_REST_POSITION);
-        kickerStates.reset();
-        kickerStates.setState(kickerStates.INIT); //makes sure robot is on init
-        while (isTest() && isEnabled()) {
-            kickerStates.stateMachine();
-        }*/
+         kickerStates.reset();
+         kickerStates.setState(kickerStates.INIT); //makes sure robot is on init
+         while (isTest() && isEnabled()) {
+         kickerStates.stateMachine();
+         }*/
     }
 }
